c30c75a9067be0b6fad143369d5a10e92745f680
ROHIT DATTATARY GAYKHE <96008151+rohitgaykhe06@users.noreply.github.com>
2022-07-23 19:20:48 +0530

    Update test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,18 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-
+    
     cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.sel.value=13
+    dut.inp13.value=2
+    dut.inp12.value=1
+
+    await Timer(2, units='ns')
+    
+    assert dut.out.value ==dut.inp13.value, "Mux result is incorrect: {out} != {inp12}, expected value={inp13}".format(
+            out=dut.out.value,inp12=dut.inp12.value,inp13=dut.inp13.value)
+
+
+
+
+
